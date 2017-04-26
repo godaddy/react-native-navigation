336907584d9f04b022114f5f777135113b4549a3
Dustin Savery <dustin@paintedpixels.design>
2017-04-26 13:11:12 -0700

    Shared element patch (#9)
    * Fixing issue with SharedElementTransition
    
    * Fixing export of SharedElement, adding children for rendering
    
    * Reverting removal of sharedElementTransition.js, adding children to iOS implementation

--- src/views/sharedElementTransition.ios.js
+++ src/views/sharedElementTransition.ios.js
@@ -1,10 +1,14 @@
-import React, {Component, View} from 'react';
+import React, {Component} from 'react';
 import {
-  requireNativeComponent
+  View
 } from 'react-native';
 
 export default class SharedElementTransition extends Component {
   render() {
-    return <View />;
+    return (
+      <View>
+        {this.props.children}
+      </View>
+    );
   }
 }
